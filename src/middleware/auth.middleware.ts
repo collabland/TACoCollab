65ef7fc2212030336b64bf6cf13c2fdf2f18603d
RohitKM26 <rohit@collab.land>
2026-01-22 19:11:38 +0530

    refactor: remove console log from authMiddleware for cleaner code

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -7,7 +7,6 @@ const API_KEY = process.env.API_KEY;
 
 export function authMiddleware(req: Request, res: Response, next: NextFunction) {
   const apiKey = req.headers['x-api-key'];
-  console.log(apiKey, API_KEY);
   if (!apiKey || apiKey !== API_KEY) {
     return res.status(401).json({ error: 'Unauthorized: Invalid API Key' });
   }
